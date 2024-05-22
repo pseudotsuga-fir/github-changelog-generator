f60dc5b12316b4d4280299453789c88e348cb7f3
Andrew Hepworth <hepworta@oregonstate.edu>
2024-05-21 22:16:30 -0700

    update changelog generator spec to include more options behavior specs

--- spec/github_changelog_generator_spec.rb
+++ spec/github_changelog_generator_spec.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+require 'byebug'
 
 RSpec.describe GitHubChangelogGenerator::ChangelogGenerator do
   describe "#run" do
@@ -26,6 +27,46 @@ RSpec.describe GitHubChangelogGenerator::ChangelogGenerator do
       let(:arguments) { ["--output", ""] }
       it "puts the complete output path to STDOUT" do
         expect { instance.run }.to output(/content/).to_stdout
+      end
+    end
+
+    context "when date format is given in argument" do
+      let(:arguments) { ["--date-format", "%YYYY-%m-%d"] }
+      it "calls generator with the given date format" do
+        expect(instance.get_options[:date_format]).to eq("%YYYY-%m-%d")
+      end
+    end
+
+    context "when date format is not given in argument" do
+      it "calls generator with the default date format" do
+        expect(instance.get_options[:date_format]).to eq("%Y-%m-%d")
+      end
+    end
+
+    # context "when invalid date format is given in argument" do
+    #   let(:arguments) { ["--date-format", "invalid"] }
+    #   it "raises an error" do
+    #     expect { instance.run }.to raise_error(ArgumentError, /invalid date format/)
+    #   end
+    # end
+
+    context "when header label is given in argument" do
+      let(:arguments) { ["--header-label", "Header Label"] }
+      it "calls generator with the given header label" do
+        expect(instance.get_options[:header]).to eq("Header Label")
+      end
+    end
+
+    context "when header label is not given in argument" do
+      it "calls generator with the default header label" do
+        expect(instance.get_options[:header]).to eq("# Changelog")
+      end
+    end
+
+    context "when no issues is specified" do
+      let(:arguments) { ["--no-issues", ""] }
+      it "calls generator with the no issues option" do
+        expect(instance.get_options[:issues]).to eq(false)
       end
     end
   end
